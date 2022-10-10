025176944e68d2180418cc66f9bcf22693d9eeed
Thomas Raducha <thomasraducha@gmail.com>
2022-10-10 20:55:52 +0200

    save 3

--- lib/03_basics.rb
+++ lib/03_basics.rb
@@ -11,47 +11,14 @@ def who_is_bigger(a, b, c)
   end
   
   def reverse_upcase_noLTA(str)
-  
     arr = []
   
-    if str == "Tries this at Home, Kids"
-      str.split(" ").each_with_index do |s, index|
-        newString = s
-        if index != 0
-          newString = s.downcase.sub('t', '').sub('a', '')
-          arr.push(newString.upcase)
-        else
-          arr.push(newString.upcase)
-        end
-      
-      end
-    elsif str == "Ponies loves carrots"
-      str.split(" ").each_with_index do |s, index|
-        newString = s
-          newString = s.downcase.sub('a', '').sub('l', '').sub('t', '')
-          arr.push(newString.upcase)
-      end
-    end
-    if str == "qwertyuiopasdfghjkl;zxcvbn"
-      return str.sub('a', '').sub('l', '').sub('t', '').reverse.upcase
-    else
-      return arr.join(" ").reverse
-    end
-  
-end
-
-=begin
-puts reverse_upcase_noLTA("Tries this at Home, Kids")
-
-
-  def reverse_upcase_noLTA(str)
-    arr = []
-  
-    str.split(" ").each_with_index do |s, index|
+    str.split(" ").each do |s|
         newString = s.sub('a', '').sub('l', '').sub('t', '')
         arr.push(newString.upcase)
     end
   
     return arr.join(" ").reverse
-  end 
-=end
+  end
+
+reverse_upcase_noLTA("Tries this at Home, Kids")
--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -1,1 +1,18 @@
-def echo()
+def echo(hello)
+     hello
+end
+def echo(bye)
+    bye
+end
+
+def shout(hello)
+    hello.upcase
+end
+
+def repeat (hello, i=2)
+    hello*(" #{hello}" * (i-1))
+end
+
+def start_of_word(start)
+    
+end
